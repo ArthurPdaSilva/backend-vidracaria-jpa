a59c8971c896ace9d262badf34d49a5c9de42b87
Wesley_ <wesley.souza@academico.ifpb.edu.br>
2022-09-15 22:03:22 -0300

    update empregado

--- src/main/br/com/bdii/entities/Empregado.java
+++ src/main/br/com/bdii/entities/Empregado.java
@@ -1,16 +1,21 @@
 package br.com.bdii.entities;
 
-import org.hibernate.annotations.Table;
+
+import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.UniqueConstraint;
 
-//@Entity(name = "EMPREGADO")
-public class Empregado {
+@Entity(name = "EMPREGADO")
+public class Empregado implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -35,6 +40,64 @@ public class Empregado {
 	
 	@Column(nullable = false, name = "TIPO_EMPREGADO")
 	private String tipo;
+
+	public Empregado() {}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getHorarioServico() {
+		return horarioServico;
+	}
+
+	public void setHorarioServico(String horarioServico) {
+		this.horarioServico = horarioServico;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 	
 	
 }
