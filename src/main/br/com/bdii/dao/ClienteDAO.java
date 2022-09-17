c6a9180060c00cf2b5de38d4f6e47a5082325d20
Wesley_ <wesley.souza@academico.ifpb.edu.br>
2022-09-17 15:20:19 -0300

    updates importantes

--- src/main/br/com/bdii/dao/ClienteDAO.java
+++ src/main/br/com/bdii/dao/ClienteDAO.java
@@ -1,5 +1,98 @@
 package br.com.bdii.dao;
 
-public class ClienteDAO {
+import java.util.List;
 
+import br.com.bdii.entities.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+
+public class ClienteDAO extends DAO{
+	public void save(Cliente cliente) throws Exception {
+		EntityManager em = getEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		try {
+			em.persist(cliente);
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+			throw new Exception("Ocorreu algum erro ao tentar inserir o usuário.", e);
+		} finally {
+			em.close();
+		}
+	}
+
+	public Cliente update(Cliente cliente) throws Exception {
+		EntityManager em = getEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		Cliente resultado = cliente;
+		try {
+			resultado = em.merge(cliente);
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+			throw new Exception("Ocorreu algum erro ao tentar atualizar o usuário.", e);
+		} finally {
+			em.close();
+		}
+		return resultado;
+	}
+
+	public void delete(Cliente cliente) throws Exception {
+		EntityManager em = getEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		try {
+			cliente = em.find(Cliente.class, cliente.getIdCliente());
+			em.remove(cliente);
+			transaction.commit();
+		} catch (Exception pe) {
+			pe.printStackTrace();
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+			throw new Exception("Ocorreu algum erro ao tentar remover o usuário.", pe);
+		} finally {
+			em.close();
+		}
+	}
+
+	public Cliente getByID(int userId) throws Exception {
+		EntityManager em = getEntityManager();
+		Cliente resultado = null;
+		try {
+			resultado = em.find(Cliente.class, userId);
+		} catch (Exception pe) {
+			pe.printStackTrace();
+			throw new Exception("Ocorreu algum erro ao tentar recuperar o usuário com base no ID.", pe);
+		} finally {
+			em.close();
+		}
+
+		return resultado;
+	}
+
+	public List<Cliente> getAll() throws Exception {
+		EntityManager em = getEntityManager();
+		List<Cliente> resultado = null;
+		try {
+			TypedQuery<Cliente> query = em.createQuery("SELECT u FROM User u", Cliente.class);
+			resultado = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception("Ocorreu algum erro ao tentar recuperar todos os usuários.", e);
+		} finally {
+			em.close();
+		}
+		return resultado;
+	}
 }
--- src/main/br/com/bdii/main/MainSave.java
+++ src/main/br/com/bdii/main/MainSave.java
@@ -1,10 +1,26 @@
 package br.com.bdii.main;
 
+import br.com.bdii.dao.ClienteDAO;
+import br.com.bdii.entities.Cliente;
+
 public class MainSave {
 
 	public static void main(String[] args) {
 		
-
+		// àrea de testes
+		ClienteDAO cDao = new ClienteDAO();
+		Cliente c = new Cliente();
+		c.setCpf("12345678901");
+		c.setEmail("teste@rotimei.com");
+		c.setNome("patricio");
+		
+		try {
+			cDao.save(c);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
